e92f7ccc8f7d2a5cd9b7773709f19b0ab59886bf
polypixeldev <me@sfernandez.dev>
2025-01-02 20:47:44 -0500

    stop using events api

--- index.js
+++ index.js
@@ -23,67 +23,68 @@ expressApp.get("/refresh", async (req, res) => {
   res.status(200).send("the backlog has been refreshed!");
 });
 
-app.event("star_added", async (e) => {
-  await prisma.item.upsert({
-    where: {
-      ts: e.payload.item.message.ts,
-    },
-    create: {
-      ts: e.payload.item.message.ts,
-      timeAdded: new Date(),
-      timeRemoved: null,
-    },
-    update: {
-      timeAdded: new Date(),
-      timeRemoved: null,
-    },
-  });
-
-  count++;
-  updateCount();
-});
-
-app.event("star_removed", async (e) => {
-  try {
-    await prisma.item.update({
-      where: {
-        ts: e.payload.item.message.ts,
-      },
-      data: {
-        timeRemoved: new Date(),
-      },
-    });
-  } catch (e) {
-    console.error(e);
-  }
-
-  count--;
-  updateCount();
-});
-
 async function refreshTasks() {
-  const authData = new FormData();
-  authData.set("token", process.env.SLACK_CLIENT_TOKEN);
-  authData.set("_x_app_name", "client");
-  authData.set("_x_reason", "saved-api/savedList");
-  authData.set("_x_mode", "online");
-  authData.set("_x_sonic", "true");
+  const data = new FormData();
+  data.set("token", process.env.SLACK_CLIENT_TOKEN);
+  data.set("_x_app_name", "client");
+  data.set("_x_reason", "saved-api/savedList");
+  data.set("_x_mode", "online");
+  data.set("_x_sonic", "true");
+  data.set("filter", "saved");
 
   await fetch(
     "https://hackclub.slack.com/api/saved.list?_x_id=829ee587-1712708450.151&_x_csid=JmcOvVbEE0U&slack_route=T0266FRGM&_x_version_ts=1712706141&_x_frontend_build_type=current&_x_desktop_ia=4&_x_gantry=true&fp=22",
     {
       method: "POST",
-      body: authData,
+      body: data,
       headers: {
         Cookie: process.env.SLACK_COOKIE,
       },
     },
   )
     .then((res) => res.json())
-    .then((data) => {
+    .then(async (data) => {
       if (data.ok) {
         count =
           data.counts.uncompleted_count + data.counts.uncompleted_overdue_count;
+
+        const savedIds = data.saved_items.map((i) => i.ts);
+        for (const item in data.saved_items) {
+          await prisma.item.upsert({
+            where: {
+              ts: item.ts,
+            },
+            create: {
+              ts: item.ts,
+              timeAdded: new Date(e.date_created * 1000),
+              timeRemoved: null,
+            },
+            update: {
+              timeAdded: new Date(e.date_created * 1000),
+              timeRemoved: null,
+            },
+          });
+        }
+
+        const currentItems = await prisma.item.findMany({
+          where: {
+            timeRemoved: null,
+          },
+        });
+
+        const removedItems = currentItems.filter((i) => !savedIds.includes(i));
+
+        for (const item in removedItems) {
+          await prisma.item.update({
+            where: {
+              ts: item,
+            },
+            data: {
+              timeRemoved: new Date(),
+            },
+          });
+        }
+
         updateCount();
       }
     });
@@ -135,6 +136,8 @@ async function updateCount() {
     title: `Avg time per task: ${await calculateAverage()}`,
   });
 }
+
+setInterval(refreshTasks, 60 * 1000);
 
 (async () => {
   await refreshTasks();
